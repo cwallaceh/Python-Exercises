6dbf7bc77471b330e7b4f9b48aad47e44a876e69
cwallaceh <carl.wallace@konfio.mx>
2019-01-14 11:42:44 -0600

    README.md changes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Python Exercises 
 
 A repo with the most common algorithms and their implementations in python
+
+The *timing* function is used to time the algorithms behavior
--- rabin_karp_algorithm.py
+++ rabin_karp_algorithm.py
@@ -1,20 +1,40 @@
-# Rabin-Karp Algorithm for Pattern Searching
+# Rabin-Karp Algorithm for Pattern Searching using a simple Hash Table
 
 # Given a text txt[0..n-1] and a pattern pat[0..m-1],
 # write a function search(char pat[], char txt[])that prints all occurrences of pat[] in txt[].
 # You may assume that n > m.
 
+
 txt = "BACDGABCDABCD"
 pat = "ABCD"
 
+# d is the number of characters in the input alphabet
+d = 256
 
-def rabin_karp(txt, pat):
+
+class HashTable():
+
+    def __init__(self, n=10):
+        self.hash_table = self.create_hash_table(n)
+
+    def create_hash_table(self, n):
+        """Function to create hash table with 'n' values"""
+        hash_table = [[] for _ in range(n)]
+        return hash_table
+
+    def hashing_func(self, key):
+        return hash(key) % len(self.hash_table)
+
+
+def rabin_karp(txt, pat, d):
     """O(n) complexity"""
     # Traverse the txt using a len(pat) window
+
+    _hash_ = HashTable(d)
     for new in [txt[i:i + len(pat)] for i in range(len(txt) - len(pat) + 1)]:
         # Check if the hash value is the same
-        if hash(new) == hash(pat):
+        if _hash_.hashing_func(new) == _hash_.hashing_func(pat):
             print(new)
 
 
-rabin_karp(txt, pat)
+rabin_karp(txt, pat, d)
